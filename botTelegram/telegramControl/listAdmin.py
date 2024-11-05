15de99013302e8caab67208db21147c38bed87ac
Semai Mohamed <126528399+Semai-Mohamed@users.noreply.github.com>
2024-11-05 03:41:54 +0100

    Update listAdmin.py

--- botTelegram/telegramControl/listAdmin.py
+++ botTelegram/telegramControl/listAdmin.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import CommandHandler
-async def list_admins(update:Update,chat):
+async def list_admins(update:Update,chat): 
     admins = await chat.get_administrators()
     admin_list = '\n'.join([f'{admin.user.username}'for admin in admins])
     print("mohamed")
